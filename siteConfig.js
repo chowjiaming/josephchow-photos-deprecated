27d4d2c51889f3560b5a1f529d11a7ca3b288466
joefstack <joefstack@gmail.com>
2019-10-16 12:38:28 -0400

    preliminary config setup

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Imed Adel',
-  twitter: 'Imed_Adel'
+  name: "Joseph Chow",
+  shortName: "josephchow-photos",
+  description: `A personal photo blog featuring photos I've taken with my Pixel2XL`,
+  url: "https://josephchow.photos",
+  prefix: "/",
+  author: "Joseph Chow",
+  twitter: "Joefstack",
 }
